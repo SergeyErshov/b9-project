71c59d2a4e88f562c91a86f3f0a651d50b73ced1
SergeyErshov <s.ershov.shoko@gmail.com>
2022-12-13 14:16:22 +0300

    add s3 directive

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -5,6 +5,20 @@ terraform {
       version = "0.82.0"
     }
   }
+
+  required_version = ">= 0.13"
+
+  # Настройка доступа к S3 внешнему хранилищу State-файла
+
+  backend "s3" {
+    endpoint = "storage.yandexcloud.net"
+    bucket   = "test-bucket-esm"
+    region   = "ru-central1-a"
+    key      = "tf/b9-proj.tfstate"
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+  }
 }
 
 provider "yandex" {
