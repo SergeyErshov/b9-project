4006d52e8518998c8cf8e38b5d128637311a77fc
SergeyErshov <s.ershov.shoko@gmail.com>
2022-12-13 14:17:12 +0300

    some changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,27 +1,10 @@
 module "yandex_instance_1" {
   source                = "./modules/create_vm"
-  disk_size             = "20"
-  instance_family_image = "ubuntu-2004-lts"
+  disk_size             = "64"
+  cores = "4"
+  memory = "4"
+  instance_family_image = "Centos-7"
   instance_subnet_name  = "a-ru-central1-a"
-  instance_name         = "vm1"
-  zone_name             = "ru-central1-a"
-}
-
-
-module "yandex_instance_2" {
-  source                = "./modules/create_vm"
-  disk_size             = "20"
-  instance_family_image = "ubuntu-2004-lts"
-  instance_subnet_name  = "a-ru-central1-a"
-  instance_name         = "vm2"
-  zone_name             = "ru-central1-a"
-}
-
-module "yandex_instance_3" {
-  source                = "./modules/create_vm"
-  disk_size             = "20"
-  instance_family_image = "centos-7"
-  instance_subnet_name  = "a-ru-central1-a"
-  instance_name         = "vm3"
+  instance_name         = "vm01"
   zone_name             = "ru-central1-a"
 }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -10,24 +10,3 @@ output "external_ipaddr_vm01" {
   value       = module.yandex_instance_1.external_ipaddr_vm
 }
 
-
-output "internal_ipaddr_vm02" {
-  description = "internal ip 0f VM"
-  value       = module.yandex_instance_2.internal_ipaddr_vm
-}
-
-output "external_ipaddr_vm02" {
-  description = "external ip of VM"
-  value       = module.yandex_instance_2.external_ipaddr_vm
-}
-
-output "internal_ipaddr_vm03" {
-  description = "internal ip 0f VM"
-  value       = module.yandex_instance_3.internal_ipaddr_vm
-}
-
-output "external_ipaddr_vm03" {
-  description = "external ip of VM"
-  value       = module.yandex_instance_3.external_ipaddr_vm
-}
-
