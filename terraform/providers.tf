1232d7b937fc03cdfc481c499192adc12c053854
SergeyErshov <s.ershov.shoko@gmail.com>
2022-12-13 14:51:24 +0300

    some changes

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -21,10 +21,4 @@ terraform {
   }
 }
 
-provider "yandex" {
-  service_account_key_file = file("~/authorized_key.json")
-  cloud_id                 = "b1ga0bs8pioqsr6ck6kk"
-  folder_id                = "b1gb2sfmo75im8haari2"
-  zone                     = "ru-central1-a"
-}
 
